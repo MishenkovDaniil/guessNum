c246ad8034ff43c55c470d2843e1db37885cfa01
Mishenkov Daniil <mishenkovdaniil@mail.ru>
2025-02-17 18:50:38 +0300

    made level choose and checking input functions'

--- main.go
+++ main.go
@@ -5,31 +5,68 @@ import (
 	"math/rand"
 )
 
-const GreetingMsg = "Привет, это игра guessNum, где тебе потребуется угадать загаданное мною число. Чтобы начать просто сделайте первое предположение!"
+const greetingMsg = "Привет, это игра guessNum, где тебе потребуется угадать загаданное мною число!"
+const startMsg = "Уровень выбран! Чтобы начать просто сделайте первое предположение!"
+const easyLevel = "легкий"
+const mediumLevel = "средний"
+const hardLevel = "сложный"
+const easyLevelMax = 20
+const mediumLevelMax = 50
+const hardLevelMax = 100
+
+func readNum(num *int) error {
+	_, err := fmt.Scan(num)
+
+	if err != nil {
+		fmt.Println("Введите корректное число!")
+	}
+	return err
+}
+
+func generateNum() int {
+	fmt.Printf("Выберите уровень сложности:\n%v\n%v\n%v\n", easyLevel, mediumLevel, hardLevel)
+	var level string
+	var num int
+	for {
+		_, err := fmt.Scan(&level)
+		if err != nil {
+			fmt.Println("Введите корректное значение!")
+			continue
+		}
+
+		switch level {
+		case easyLevel:
+			num = rand.Intn(easyLevelMax)
+		case mediumLevel:
+			num = rand.Intn(mediumLevelMax)
+		case hardLevel:
+			num = rand.Intn(hardLevelMax)
+		default:
+			fmt.Println("Введите корректное значение!")
+			continue
+		}
+		break
+	}
+	return num
+}
 
 func main() {
-	fmt.Println(GreetingMsg)
-	myNum := rand.Intn(100)
-	i := 1
+	fmt.Println(greetingMsg)
+	myNum := generateNum()
+	fmt.Println(startMsg)
+
+	var guessedNum, tries int
 	for {
-		var guessedNum int
-		_, err := fmt.Scan(&guessedNum)
-		if err != nil {
-			fmt.Println("Введите корректное число!")
-			// log.Fatal(err)
+		if readNum(&guessedNum) != nil {
+			continue
 		}
-		// if n > 1 {
-		// 	continue
-		// }
-		// guessedNum, err := strconv.Atoi(msg)
+		tries++
 		if myNum > guessedNum {
 			fmt.Println("Слишком мало!")
-			i++
 		} else if myNum < guessedNum {
 			fmt.Println("Слишком много!")
-			i++
 		} else {
-			fmt.Printf("Поздравляем! Вы угадали число за %v попыток\n", i)
+			fmt.Printf("Поздравляем! Вы угадали число за %v попыток\n", tries)
 			break
 		}
 	}
